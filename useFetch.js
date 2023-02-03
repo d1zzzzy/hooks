256303a4f0a88fbbf35c3592369309584837bf03
d1zzzy <80456522+d1zzzzy@users.noreply.github.com>
2023-02-03 15:15:53 +0800

    (feat)[useFetch] add useFetch.js file

---
+++ useFetch.js
@@ -0,0 +1,31 @@
+import { useCallback, useMemo } from "react"
+import useAsync from "./useAsync"
+
+const DefaultOptions = {
+  headers: {
+    "Content-Type": "application/json"
+  }
+}
+
+function useFetch(url, options = {}, dependencies = []) {
+  const abortController = useMemo(() => new AbortController(), [])
+
+  const abort = useCallback(() => abortController.abort(), [abortController])
+
+  const { loading, error, value } = useAsync(() => {
+    return fetch(
+      url,
+      {
+        signal: abortController.signal,
+        ...DefaultOptions,
+        ...options,
+      }
+    ).then(response => {
+      if (response.ok) return response.json()
+
+      return response.json().then(json => Promise.reject(json))
+    }, dependencies)
+  })
+
+  return { loading, error, value, abort }
+}
